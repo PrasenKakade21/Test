5ece49ec1d8b09266ce090e7cbc114a3e18e592b
Prasen Kakade <65662813+PrasenKakade21@users.noreply.github.com>
2026-01-05 18:55:26 +0530

    Add Leetcode 1003. Check If Word Is Valid After Substitutions c++

---
+++ check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,10 @@
+# 1003. Check If Word Is Valid After Substitutions
+# https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/
+# Language: c++
+
+class Solution {class Solution {
+public:public:
+    bool isValid(string s) {    bool isValid(string s) {
+                
+    }    }
+};};
