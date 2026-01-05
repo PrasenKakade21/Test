99201b866407fc761e6d74071806c1d70a737bd7
Prasen Kakade <65662813+PrasenKakade21@users.noreply.github.com>
2026-01-05 20:45:47 +0530

    Add solution: ICE CREAM

---
+++ codechef/ICE_CREAM.cpp
@@ -0,0 +1,13 @@
+/*
+ * Problem: ICE CREAM
+ * URL: https://www.codechef.com/START132D/problems/ICE_CREAM
+ * Platform: codechef
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+
+}
