f860df596241c1add88f9ca1872f477a20a26488
Prasen Kakade <65662813+PrasenKakade21@users.noreply.github.com>
2026-01-05 18:58:00 +0530

    Add Leetcode 1003: check-if-word-is-valid-after-substitutions [java]

---
+++ check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,9 @@
+# 1003. Check If Word Is Valid After Substitutions
+# https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/
+# Language: java
+
+class Solution {class Solution {
+    public boolean isValid(String s) {    public boolean isValid(String s) {
+                
+    }    }
+}}
