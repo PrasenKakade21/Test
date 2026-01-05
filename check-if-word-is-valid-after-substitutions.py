694b39708737b39a9947a1eefef1ff032d3135ea
Prasen Kakade <65662813+PrasenKakade21@users.noreply.github.com>
2026-01-05 18:46:10 +0530

    Update solution for 1003. Check If Word Is Valid After Substitutions
    #leetcode #problem1003

--- check-if-word-is-valid-after-substitutions.py
+++ check-if-word-is-valid-after-substitutions.py
@@ -1,10 +1,10 @@
-# check-if-word-is-valid-after-substitutions
+# 1003. Check If Word Is Valid After Substitutions
 # https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/
 # Language: python
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        s1 = ""
-        while s1 != s:
-            s, s1 = s.replace("abc", ""), s
-        return s == ""
+class Solution:class Solution:
+    def isValid(self, s: str) -> bool:    def isValid(self, s: str) -> bool:
+        s1 = ""        s1 = ""
+        while s1 != s:        while s1 != s:
+            s, s1 = s.replace("abc", ""), s            s, s1 = s.replace("abc", ""), s
+        return s == ""        return s == ""
